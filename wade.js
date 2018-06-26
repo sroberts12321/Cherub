b3138a3b4f17c24476dcb0b90365f254969ba938
wade welch <wade.c.welch@gmail.com>
2018-06-26 14:46:55 -0500

    skeleton5

--- wade.js
+++ wade.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express()
 
+var bcrypt = require('bcrypt')
 let models = require('./models')
 const exphbs = require('express-handlebars')
 let session = require('express-session')
@@ -9,27 +11,76 @@ let bodyParser = require('body-parser')
 
 
 
-const { Pool } = require('pg');
+const { Pool } = require('pg')
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
 })
 
-express()
-.engine('handlebars',exphbs())
-.use(bodyParser.urlencoded({ extended: false }))
-.use(session({
+app.engine('handlebars',exphbs())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(session({
   secret: 'doge',
   resave: false,
   saveUninitialized: false
 }))
 
-.use(express.static(path.join(__dirname, 'public')))
-.set('views', path.join(__dirname, 'views'))
-.set('view engine', 'handlebars')
-.get('/', (req, res) => res.render('index'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'handlebars')
 
-.get('/db', async (req, res) => {
+app.get('/login', (req, res) => res.render('login'))
+
+app.post('/login', (req, res) => {
+console.log('login REEEEEE')
+console.log(req.body.email)
+console.log(req.body.password)
+bcrypt.hash(req.body.password, 10, function(err, hash) {
+// Store hash in your password DB.
+console.log(hash)
+})
+
+})
+
+//verify matching username and password
+
+//set that session BOI
+
+/*
+  req.session.username = username
+  // setting the expiration date of the cookies so we can
+  // come back later even if we close the browser
+  var hour = 3600000
+  req.session.cookie.expires = new Date(Date.now() + hour)
+  req.session.cookie.maxAge = hour
+
+  res.render('login')
+} */
+
+app.get('/register', (req, res) => res.render('register'))
+
+app.post('/register', (req, res) => {
+
+  console.log(req.body.username)
+  console.log(req.body.password)
+  console.log(req.body.email)
+  console.log(req.body.dob)
+  console.log(req.body.gender)
+  console.log(req.body.sexpref)
+
+  //check if email already exists in users table
+
+
+
+
+  bcrypt.hash(req.body.password, 10, function(err, hash) {
+  // Store hash in your password DB.
+  console.log(hash)
+})
+  res.redirect('/register')
+})
+
+/* app.get('/db', async (req, res) => {
   try {
     const client = await pool.connect()
     const result = await client.query('SELECT * FROM test_table');
@@ -39,5 +90,6 @@ express()
     console.error(err);
     res.send("Error " + err);
   }
-})
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+}) */
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
