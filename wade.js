5b47a1af718d47830ea1b24e2000c828bae9d3a9
wade welch <wade.c.welch@gmail.com>
2018-06-27 11:11:03 -0500

    reworked wade.js to appease the angry sequelize, again2

--- wade.js
+++ wade.js
@@ -21,9 +21,9 @@ const pool = new Pool({
 })
 
 //app.use(express.favicon());
-app.use(express.logger('dev'));
+//app.use(express.logger('dev'));
 //app.use(express.bodyParser());
-app.use(express.methodOverride());
+//app.use(express.methodOverride());
 //app.use(app.router);
 
 app.engine('handlebars',exphbs())
