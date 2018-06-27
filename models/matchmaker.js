f3d0c37d11aa3e44c9fbb19f41cf6678dc3678b4
wade welch <wade.c.welch@gmail.com>
2018-06-27 18:04:28 -0500

    added matchmaker and nomination models, still needs alterations to index

---
+++ models/matchmaker.js
@@ -0,0 +1,17 @@
+'use strict';
+ module.exports = (sequelize, DataTypes) => {
+   var Matchmaker = sequelize.define('Matchmaker', {
+     //id for matchmaker/nominee relationship
+     matchmakernomineeid: {
+       type: DataTypes.integer, //can be UUID
+       primaryKey: true,
+       allowNull: false
+     }
+     //this is from userid
+     nominee: DataTypes.integer,
+     //this is from userid
+     prospect: DataTypes.integer,
+   }, {});
+
+   return Matchmaker;
+ };
---
+++ models/nomination.js
@@ -0,0 +1,18 @@
+
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Nomination = sequelize.define('Nomination', {
+    //id for matchmaker/nominee relationship
+  nomineeprospectid: {
+      type: DataTypes.integer, //can be UUID
+      primaryKey: true,
+      allowNull: false
+    }
+    //this is from userid
+    matchmaker: DataTypes.integer,
+    //this is from userid
+    nominee: DataTypes.integer,
+  }, {});
+
+  return Nomination;
+};
