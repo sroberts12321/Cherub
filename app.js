740d2f57f9c39c53a0248d46bbf2ed32dbb29068
wade welch <wade.c.welch@gmail.com>
2018-07-01 19:07:57 -0500

    priliminary approach to displaying matches 7

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ app.post('/match', (req, res) => {
 app.get('/test', (req, res) => {
 db.Nomination.findAll({where: {nomineeprospectid : req.session.userid}}).then(function(matches){
  console.log(matches.nominee)
- console.log(matches[0])
+ console.log(matches[0]['nomination'].nominee)
 db.UserProfile.findAll({where: { id: {[Op.in]: matches.nominee}}}).then(function(matchedUsers){
 res.render('test', {matchesList: matchedUsers})
   })
