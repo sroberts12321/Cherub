21b0e020b4493a61bccf223e45531a97d4c24257
wade welch <wade.c.welch@gmail.com>
2018-07-14 16:10:10 -0500

    add picture should add to profilepics table

--- models/profilepic.js
+++ models/profilepic.js
@@ -12,6 +12,9 @@ module.exports = (sequelize, DataTypes) => {
     userid: DataTypes.INTEGER,
 
   }, {});
+  Profilepic.associate = function(models) {
+    // associations can be defined here
+  };
 
   return Profilepic;
 };
